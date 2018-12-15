dc6977cf50328f2d322e9f52fc5a4228a81bad82
Joe Black <me@joeblack.nyc>
2018-12-15 14:53:50 -0500

    Minor changes, added license

--- tasks.py
+++ tasks.py
@@ -7,6 +7,26 @@ def build_docker(ctx):
     ctx.run("docker build -t joeblackwaslike/texlive:2016 docker-texlive")
 
 
+def _build_pdf(ctx):
+    ctx.run("scripts/xelatex temp")
+
+
+def _move_and_rename(ctx, file_name):
+    ctx.run(f"mv latex/temp.pdf export/{file_name}")
+
+
+def _clean_up(ctx):
+    ctx.run("rm -f latex/temp*")
+
+
+def _extract_txt(ctx, file_name):
+    ctx.run(f"pdftotext -layout export/{file_name}")
+
+
+def _preview(ctx, file_name):
+    ctx.run(f"open export/{file_name}")
+
+
 @task
 def render(ctx, data="blockchain", pty=True):
     data_path = f"data/{data}.yaml"
@@ -14,19 +34,12 @@ def render(ctx, data="blockchain", pty=True):
     # Generate latex
     doc = ResumeDocument.from_jsonresume(data_path)
     doc.export("latex/temp")
-    ctx.run("scripts/xelatex temp")
-
-    # Move and rename
     file_name = f"{doc.file_name}.pdf"
-    ctx.run(f"mv latex/temp.pdf export/{file_name}")
 
-    # clean up
-    ctx.run("rm -f latex/temp*")
-
-    # extract text to check machine parsers
-    ctx.run(f"pdftotext -layout export/{file_name}")
-
-    # open to preview pdf
-    ctx.run(f"open export/{file_name}")
+    _build_pdf(ctx)
+    _move_and_rename(ctx, file_name)
+    _clean_up(ctx)
+    _extract_txt(ctx, file_name)
+    _preview(ctx, file_name)
 
     print(f"Finished generating: {file_name}")
